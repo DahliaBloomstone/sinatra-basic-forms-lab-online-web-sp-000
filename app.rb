d561bd246500904c1c0d6b05d968617b1d0c444a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 20:46:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ class App < Sinatra::Base
 
   get '/' do
   erb :index
-end
+  end
 
 get '/new' do
   erb :create_puppy
-end
+  end
 
 post '/puppy' do
   @puppy = Puppy.new(params[:name], params[:breed], params[:age])
 erb :display_puppy
-end
+  end
 
 end
