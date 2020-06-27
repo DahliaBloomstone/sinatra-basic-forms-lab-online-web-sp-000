fa6456d8b036e4a7d4f685f706ddaa1ca257cb87
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 20:47:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ erb :create_puppy
   end
 
 post '/puppy' do
-  @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+@puppy = Puppy.new(params[:name], params[:breed], params[:age])
 erb :display_puppy
   end
 
